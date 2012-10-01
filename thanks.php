196b10270707b7d25d357f0a6dad011673601f57
Aaron Ott <aaron.ott@gmail.com>
2012-09-30 20:28:47 -0600

    Writes thanks.

--- thanks.php
+++ thanks.php
@@ -1,8 +1,32 @@
 #!/usr/bin/env php
 <?php
 
-if (!isset($argv[1])) {
+define('DBDIR',  $_SERVER['HOME'] . '/Dropbox/.thanks');
+
+$db_file = DBDIR . '/thanks.log';
+$week = array(
+  "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+);
+
+$week_starts = "Thursday";
+
+
+
+if (!isset($argv[2]) && (!isset($argv[1]) || $argv[1] != "report")) {
   die("Usage: $argv[0] <name: reason>\n");
+}
+
+if ($argv[1] == 'report') {
+
+}
+else {
+  if (!writetodb($argv[1], $argv[2])) {
+    msg("There was a problem writing to the dbfile: $db_file.", 'ERROR');
+  }
+}
+
+function printReport() {
+
 }
 
 function writetodb($name, $reason) {
